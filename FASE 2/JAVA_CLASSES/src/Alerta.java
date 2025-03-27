832ad4f3c74d6888516e9b0c0df669ced306b899
Matheus Siqueira <matheu.f.siqueira@gmail.com>
2025-03-27 15:46:56 -0300

    MA - Criei as classes Alerta e CriptoAtivo

--- FASE 2/JAVA_CLASSES/src/Alerta.java
+++ FASE 2/JAVA_CLASSES/src/Alerta.java
@@ -1,3 +1,57 @@
 // MATHEUS
 public class Alerta {
+    private int id;
+    private Usuario usuario;
+    private CriptoAtivo cripto;
+    private String condicao;
+    private String mensagem;
+    private boolean ativo;
+
+    //Construtor
+    public Alerta(int id, Usuario usuario, CriptoAtivo cripto, String condicao, String mensagem, boolean ativo) {
+        this.id = id;
+        this.usuario = usuario;
+        this.cripto = cripto;
+        this.condicao = condicao;
+        this.mensagem = mensagem;
+        this.ativo = ativo;
+    }
+
+    //Metodo para verificar condicao do alerta
+    public boolean verificarCondicao(double precoAtual) {
+        return condicao.equals("acima") && precoAtual > cripto.getPrecoAtual() ||
+                condicao.equals("abaixo") && precoAtual < cripto.getPrecoAtual();
+    }
+
+    //Metodo pare enviar notificacao
+    public void enviarNotificacao() {
+        if (ativo) {
+            System.out.println("Notificação para " + usuario.getNome() + ": " + mensagem);
+        }
+    }
+
+    //Getters e Setters
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public Usuario getUsuario() {return usuario;}
+
+    public void setUsuario(Usuario usuario) {this.usuario = usuario;}
+
+    public CriptoAtivo getCripto() {return cripto;}
+
+    public void setCripto(CriptoAtivo cripto) {this.cripto = cripto;}
+
+    public String getCondicao() {return condicao;}
+
+    public void setCondicao(String condicao) {this.condicao = condicao;}
+
+    public String getMensagem() {return mensagem;}
+
+    public void setMensagem(String mensagem) {this.mensagem = mensagem;}
+
+    public boolean isAtivo() {return ativo;}
+
+    public void setAtivo(boolean ativo) {this.ativo = ativo;}
 }
--- FASE 2/JAVA_CLASSES/src/CriptoAtivo.java
+++ FASE 2/JAVA_CLASSES/src/CriptoAtivo.java
@@ -1,12 +1,61 @@
 // MATHEUS
 public class CriptoAtivo {
-    public double getPrecoAtual() {
-        return 0;
+    private int id;
+    private String nome;
+    private String sigla;
+    private double precoAtual;
+    private double marketCap;
+    private double volumeTransacoes;
+    private int anoCriacao;
+
+    //Construtor
+    public CriptoAtivo(int id, String nome, String sigla, double precoAtual, double marketCap, double volumeTransacoes, int anoCriacao) {
+        this.id = id;
+        this.nome = nome;
+        this.sigla = sigla;
+        this.precoAtual = precoAtual;
+        this.marketCap = marketCap;
+        this.volumeTransacoes = volumeTransacoes;
+        this.anoCriacao = anoCriacao;
     }
 
-    public String getNome() {
+    //Metodo para atualizar o preço
+    public void atualizarPreco(double novoPreco) {
+        this.precoAtual = novoPreco;
     }
 
-    public String getSigla() {
+    //Metodo para o calculo do preço com base em um valor anterior
+    public double calcularVariacao(double precoAnterior) {
+        if (precoAnterior == 0) {
+            return 0;
+        }
+        return ((precoAtual - precoAnterior) / precoAnterior) * 100;
     }
+
+    //Getters e Setters
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getNome() {return nome;}
+
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getSigla() {return sigla;}
+
+    public void setSigla(String sigla) {this.sigla = sigla;}
+
+    public double getPrecoAtual() {return precoAtual;}
+
+    public double getMarketCap() {return marketCap;}
+
+    public void setMarketCap(double marketCap) {this.marketCap = marketCap;}
+
+    public double getVolumeTransacoes() {return volumeTransacoes;}
+
+    public void setVolumeTransacoes(double volumeTransacoes) {this.volumeTransacoes = volumeTransacoes;}
+
+    public int getAnoCriacao() {return anoCriacao;}
+
+    public void setAnoCriacao(int anoCriacao) {this.anoCriacao = anoCriacao;}
 }
